02dfa333619d18a29acca5cfa54e415b1a7fd63a
bino <33786611+lbeeno09@users.noreply.github.com>
2022-10-14 21:38:16 +0900

    Delete profile.ps1

--- .config/powershell/profile.ps1
+++
@@ -1,3 +0,0 @@
-# !! Contents within this block are managed by 'conda init' !!
-(& "D:\Users\hanbi\anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
-#endregion
